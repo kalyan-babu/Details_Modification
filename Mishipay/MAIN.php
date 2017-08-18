a6c3a43daf36ab8cab9213a721ae8e0d7574fcf9
kalyan <kalyanbuddavarapu@gmail.com>
2017-08-18 12:35:20 +0530

    start with this file.

---
+++ Mishipay/MAIN.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<script type="text/javascript" src="jquery/jquery.min.js"></script>	
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+<style>
+#label
+{
+	font-weight:bold;
+}
+
+#assignment{
+	
+	font-size:40px;
+	color:green;
+	
+}
+
+#assignment1{
+	
+	font-size:25px;
+	color:red;
+	
+}
+
+
+
+</style>
+
+</head>
+
+<body>
+
+
+
+<div class="container-fluid">
+
+		<br><br><br><br><br><br><br><br><br><br><br>
+		<p align="center" id="assignment"> MISHIPAY ASSIGNMENT </p>
+		
+		<br><br>
+		<p align="center" id="assignment1">Choose The <b>INTERFACE</b> from below</p>
+		<br>
+		<p align="center">
+		<a href="Admin.php" align="center"><button type="button" class="btn btn-info btn-lg" align="center">ADMIN</button></a>
+		<a href="main_page.php" align="center"><button type="button" class="btn btn-info btn-lg" align="center">USER</button></a>
+		</p>
+
+
+  
+</div>
+</body>
+</html>
